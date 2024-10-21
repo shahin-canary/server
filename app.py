00be20f8e088a4e7b0edede9ab3b111d110ea912
shahin-canary <shahincanary@gmail.com>
2024-10-21 17:58:21 +0530

    Initial commit: add app.py, .gitignore, and img.jpg----

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from waitress import serve
+from flask import Flask, jsonify
+import base64
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>Hello</h1>"
+@app.route('/image', methods=['GET'])
+def get_image():
+    with open('img.jpg', 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    return jsonify({"image": encoded_string})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
