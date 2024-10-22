9a655ebfef9301f6a3ad3fe2875844d3b0ad9a6c
shahin-canary <shahincanary@gmail.com>
2024-10-22 10:03:24 +0530

    Fix Procfile and dependencies

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def hello():
     return "<h1>Hello</h1>"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)  
-
-
-
-# gunicorn app:app
+if __name__ == '__main__':
+    app.run()
