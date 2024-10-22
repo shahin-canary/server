ce043ef2c6fdf6db1a32897d6c276ca7b1160913
shahin-canary <shahincanary@gmail.com>
2024-10-22 16:28:18 +0530

    final push

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import base64
+import time   
 from flask import Flask, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS  
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -10,19 +11,42 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
+GEN_IMAGE_PATH = 'img.png'
+
 @app.route("/api/storyboard", methods=['GET'])
 def storyboard():
-    # Encode the image
-    image_base64 = encode_image("img.jpg")  # Update with your image path
+
+    gen_image_base64 = encode_image(GEN_IMAGE_PATH)   
+    generated_date = time.strftime("%d-%m-%Y", time.localtime())  
+    generated_time = time.strftime("%H:%M:%S", time.localtime())   
+    prompt = "Generate an image of a person on a road."
 
     # Return both the message and the image in Base64 format
     return jsonify({ 
-        "generated_image": image_base64,
-        "message": "Shahin here"
+        "prompt": prompt,   
+        "generated_image": gen_image_base64,   
+        "owner": "Shahin",  
+        "date": generated_date,  
+        "time": generated_time,  
     })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
