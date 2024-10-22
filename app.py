c1e353c5cb70922ed36f093d7726a02e9264357f
shahin-canary <shahincanary@gmail.com>
2024-10-22 13:18:12 +0530

    Add Flask app and Render configuration

--- app.py
+++ app.py
@@ -3,30 +3,35 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/api/storyboard")
-def hello():
-    return '''
-        <html>
-            <head>
-                <style>
-                    body {
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        height: 100vh;
-                        background-color: #f0f0f0;
-                    }
-                    h1 {
-                        font-size: 72px; /* Big font size */
-                        color: #ff5733; /* Color of the text */
-                    }
-                </style>
-            </head>
-            <body>
-                <h1>Hello, World!</h1>
-            </body>
-        </html>
-    '''
+def storyboard():
+    return "shahin here"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)  # Only for local development
  
+
+
+
+
+# '''
+#         <html>
+#             <head>
+#                 <style>
+#                     body {
+#                         display: flex;
+#                         justify-content: center;
+#                         align-items: center;
+#                         height: 100vh;
+#                         background-color: #f0f0f0;
+#                     }
+#                     h1 {
+#                         font-size: 72px; /* Big font size */
+#                         color: #ff5733; /* Color of the text */
+#                     }
+#                 </style>
+#             </head>
+#             <body>
+#                 <h1>Hello, World!</h1>
+#             </body>
+#         </html>
+#     '''
