3fa0a2e2975d17c272bd90dd9856e5a3565a93c4
shahin-canary <shahincanary@gmail.com>
2024-11-16 12:25:12 +0530

    commit--

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import accelerate
 import transformers
 import peft
 import uvicorn
-
+import pydantic
 # Print versions
+
 print(f"torch version: {torch.__version__}")
 print(f"diffusers version: {diffusers.__version__}")
 print(f"accelerate version: {accelerate.__version__}")
 print(f"transformers version: {transformers.__version__}")
+print(f"pydantic version: {pydantic.__version__}") 
 print(f"peft version: {peft.__version__}")
 print(f"uvicorn version: {uvicorn.__version__}")
