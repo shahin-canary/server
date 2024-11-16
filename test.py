c26a1b70755867c9446d7a932a65f518669488a8
shahin-canary <shahincanary@gmail.com>
2024-11-16 12:42:31 +0530

    commit--

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import transformers
 import peft
 import uvicorn
 import pydantic
+import fastapi
 # Print versions
 
 print(f"torch version: {torch.__version__}")
@@ -14,3 +15,7 @@ print(f"transformers version: {transformers.__version__}")
 print(f"pydantic version: {pydantic.__version__}") 
 print(f"peft version: {peft.__version__}")
 print(f"uvicorn version: {uvicorn.__version__}")
+
+print(f"uvicorn version: {fastapi.__version__}")
+print(f"uvicorn version: {fastapi.__version__}")
+print(f"uvicorn version: {fastapi.__version__}")
